ee8afb7f2895559514a7d77d9f4dba474a7b93b6
Twinkle <dim.iwanencko2014@gmail.com>
2022-09-07 19:55:26 +0300

    Ready

--- Project02/Project02/Win_Add.xaml.cs
+++ Project02/Project02/Win_Add.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace Project02
 {
@@ -19,6 +22,8 @@ namespace Project02
     /// </summary>
     public partial class Win_Add : Window
     {
+        public List<Scientists> listScientists = new List<Scientists>();
+
         public Win_Add()
         {
             InitializeComponent();
@@ -51,6 +56,87 @@ namespace Project02
             win_Add.Show();
 
             this.Close();
+        }
+
+        private void buttonAddS_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbFullname.Text != "" || tbGender.Text != "" || tbAge.Text != "" || tbFamily.Text != "" || tbСhildren.Text != "" || tbPosition.Text != "" || tbAcademicD.Text != "" )
+            {
+                XmlDocument xDoc = new XmlDocument();
+                if (File.Exists(@"C:\Scientists.xml"))
+                {
+                    xDoc.Load(@"C:\Scientists.xml");
+                    XmlElement xRoot = xDoc.DocumentElement;
+                    XmlElement userElem = xDoc.CreateElement("Scientist");
+                    XmlAttribute fullnameAttr = xDoc.CreateAttribute("fullname");
+                    XmlElement genderElem = xDoc.CreateElement("gender");
+                    XmlElement ageElem = xDoc.CreateElement("age");
+                    XmlElement familyElem = xDoc.CreateElement("family");
+                    XmlElement childernElem = xDoc.CreateElement("children");
+                    XmlElement positionElem = xDoc.CreateElement("position");
+                    XmlElement academicElem = xDoc.CreateElement("academic");
+
+                    XmlText nameText = xDoc.CreateTextNode($"{tbFullname.Text}");
+                    XmlText genderText = xDoc.CreateTextNode($"{tbGender.Text}");
+                    XmlText ageText = xDoc.CreateTextNode($"{tbAge.Text}");
+                    XmlText familyText = xDoc.CreateTextNode($"{tbFamily.Text}");
+                    XmlText childernText = xDoc.CreateTextNode($"{tbСhildren.Text}");
+                    XmlText positionText = xDoc.CreateTextNode($"{tbPosition.Text}");
+                    XmlText academicText = xDoc.CreateTextNode($"{tbAcademicD.Text}");
+
+                    //добавляем узлы
+                    fullnameAttr.AppendChild(nameText);
+                    genderElem.AppendChild(genderText);
+                    ageElem.AppendChild(ageText);
+                    familyElem.AppendChild(familyText);
+                    childernElem.AppendChild(childernText);
+                    positionElem.AppendChild(positionText);
+                    academicElem.AppendChild(academicText);
+
+                    userElem.Attributes.Append(fullnameAttr);
+                    userElem.AppendChild(genderElem);
+                    userElem.AppendChild(ageElem);
+                    userElem.AppendChild(familyElem);
+                    userElem.AppendChild(childernElem);
+                    userElem.AppendChild(positionElem);
+                    userElem.AppendChild(academicElem);
+
+                    xRoot.AppendChild(userElem);
+                    xDoc.Save(@"C:\Scientists.xml");
+                }
+                else
+                {
+
+                    XDocument xdoc = new XDocument();
+                    XElement scientist = new XElement("Scientist");
+                    XAttribute scientistsFullNameAttr = new XAttribute("fullname", $"{tbFullname.Text}");
+                    XElement scientistsGenderElem = new XElement("gender", $"{tbGender.Text}");
+                    XElement scientistsAgeElem = new XElement("age", $"{tbAge.Text}");
+                    XElement scientistsFamilyElem = new XElement("family", $"{tbFamily.Text}");
+                    XElement scientistsChildernElem = new XElement("children", $"{tbСhildren.Text}");
+                    XElement scientistsPositionElem = new XElement("position", $"{tbPosition.Text}");
+                    XElement scientistsAcademicElem = new XElement("academic", $"{tbAcademicD.Text}");
+
+                    scientist.Add(scientistsFullNameAttr);
+                    scientist.Add(scientistsGenderElem);
+                    scientist.Add(scientistsAgeElem);
+                    scientist.Add(scientistsFamilyElem);
+                    scientist.Add(scientistsChildernElem);
+                    scientist.Add(scientistsPositionElem);
+                    scientist.Add(scientistsAcademicElem);
+
+                    XElement scientists = new XElement("Scientists");
+                    scientists.Add(scientist);
+                    xdoc.Add(scientists);
+                    xdoc.Save(@"C:\Scientists.xml");
+                }
+
+                MessageBox.Show("Добавлено!");
+            }
+            else
+            {
+                MessageBox.Show("Не все поля заполнены!");
+            }
         }
     }
 }
--- Project02/Project02/obj/Debug/Project02_Content.g.i.cs
+++ Project02/Project02/obj/Debug/Project02_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/img_laboratoria_1.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("xml/info.xml")]
 
 
