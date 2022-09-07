f871a12358ec45b53f1b064b9d9484ae12511163
Twinkle <dim.iwanencko2014@gmail.com>
2022-09-07 20:59:02 +0300

    Change icon

--- Project02/Project02/obj/Debug/Project02_Content.g.i.cs
+++ Project02/Project02/obj/Debug/Project02_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("xml/info.xml")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/close.png")]
 
 
